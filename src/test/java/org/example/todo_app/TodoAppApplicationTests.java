a055e6cec0fbf1c86f21e953b4130f1a77634d5b
Roman Romanenko <capital.car7@gmail.com>
2026-01-12 09:40:05 +0100

    Initial commit

---
+++ src/test/java/org/example/todo_app/TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.todo_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
